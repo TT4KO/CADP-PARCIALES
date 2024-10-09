dc43587ba8591c0eb62f95f23ed40b2c295c9597
TT4KO <MateoQS4@hotmail.com>
2024-10-09 14:27:34 -0300

    Update copa america.pas

--- copa america.pas
+++ copa america.pas
@@ -1,6 +1,6 @@
 De dispone de una estructura de datos con la informacion de cada pais que ha participado alguna vez del evento. De 
 cada cada pais se conoce el nombre del pais, la cnatidad de participaciones en copas Americas e informacion de dichas 
-participaciones (a lo sumo 10). De cada participacion se conoce el año y en qhttps://github.com/TT4KO/CADP-PARCIALES/tree/mainue instancia finalizo(1.fase de grupo,
+participaciones (a lo sumo 10). De cada participacion se conoce el año y en instancia finalizo(1.fase de grupo,
 2.cuartos de finales, 3.semifinal, 4.final)
 A).LEA la informacion de un nuevo pais y la incorpore en la estrucuta e datos que se dispone. Del nuevo pais se lee
    el nombre del pais, la cantidad de participaciones y la informacion de cada participacion(año y en que instancia
