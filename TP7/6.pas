16d0220efc453bdee31def71d70a1774873b5f88
TT4KO <MateoQS4@hotmail.com>
2024-10-24 14:43:51 -0300

    Update 6.pas

--- TP7/6.pas
+++ TP7/6.pas
@@ -78,6 +78,24 @@ begin
         codmax2:=codigo
 end;
 
+function digitos(num: integer): boolean;
+var
+    dig, cantpar, cantimpar: integer;
+begin
+    cantimpar:=0;
+    cantpar:=0;
+    while(num <> 0) do
+    begin
+        dig:=num mod 10;
+        if(dig mod 2 = 0) then
+        cantpar:=cantpar + 1
+        else
+        cantimpar:=cantimpar + 1;
+        num:=num div 10;
+    end;
+    digitos:=cantpar > cantimpar;
+end;
+
 procedure procesar(l: lista);
 var
     v: vector;
